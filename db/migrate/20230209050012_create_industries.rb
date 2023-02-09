0ead6303fbb52a627bb1785f68fdf13112add974
Nyame-Wolf <mumenyam@gmail.com>
2023-02-09 08:12:20 +0300

    remove autoincrememnted id

--- db/migrate/20230209050012_create_industries.rb
+++ db/migrate/20230209050012_create_industries.rb
@@ -2,8 +2,9 @@ class CreateIndustries < ActiveRecord::Migration[7.0]
   def change
     create_table :industries do |t|
       t.string :name
-
+      
       t.timestamps
     end
+    change_column :industries, :id, :bigint, auto_increment: false
   end
 end
