392a2c940e08ce5054cee4bf160cb121ecffd5b5
Nyame-Wolf <mumenyam@gmail.com>
2023-02-09 08:13:17 +0300

    add self join to industry model

--- app/models/industry.rb
+++ app/models/industry.rb
@@ -1,2 +1,5 @@
 class Industry < ApplicationRecord
+  has_many :sub_sectors, class_name: 'Industry', foreign_key: 'sector_id'
+
+  belongs_to :sector, class_name: 'Industry', optional: true
 end
--- db/migrate/20230209050012_create_industries.rb
+++ db/migrate/20230209050012_create_industries.rb
@@ -2,7 +2,7 @@ class CreateIndustries < ActiveRecord::Migration[7.0]
   def change
     create_table :industries do |t|
       t.string :name
-      
+      t.references :sector, foreign_key: { to_table: :industries }
       t.timestamps
     end
     change_column :industries, :id, :bigint, auto_increment: false
