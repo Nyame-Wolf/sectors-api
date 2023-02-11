cf47e076d08fa724a2f6832afe8eb72de14c0b76
Nyame-Wolf <mumenyam@gmail.com>
2023-02-11 15:13:04 +0300

    set a cookie to enable edit of data

--- app/controllers/user_preferences_controller.rb
+++ app/controllers/user_preferences_controller.rb
@@ -17,7 +17,7 @@ class UserPreferencesController < ApplicationController
   def create
     @user_preference = UserPreference.new(user_preference_params)
     if @user_preference.save
-
+      response.set_cookie(:user_preference_id, @user_preference.id)
       render json: @user_preference, status: :created, location: @user_preference
     else
       render json: @user_preference.errors, status: :unprocessable_entity
