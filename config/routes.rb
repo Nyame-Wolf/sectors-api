0a3ba82eb3808320c7e8eee7de0582eb666a2f79
Nyame-Wolf <mumenyam@gmail.com>
2023-02-11 15:05:52 +0300

    Remove unused routes from path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :industries
+  
+  resources :industries, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
